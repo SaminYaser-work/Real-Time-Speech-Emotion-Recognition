646158744078bf143a41fa77c3b7e50ca6dc9157
Samin Yaser <saminyaserwork@gmail.com>
2022-09-03 20:30:57 +0600

    impl react-media-recorder

--- RTSER/src/App.jsx
+++ RTSER/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import useRecorder from "./hooks/useRecorder";
+import { useReactMediaRecorder } from "react-media-recorder";
 import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
 import {
@@ -13,7 +13,6 @@ import {
 } from "chart.js";
 import loadingGif from "./assets/loading.gif";
 import errorGif from "./assets/error.gif";
-// import Charts from "./components/Charts";
 
 ChartJS.register(
   CategoryScale,
@@ -69,56 +68,92 @@ export const options = {
 };
 
 function App() {
-  let [audioURL, isRecording, toggleRecording] = useRecorder();
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+    previewAudioStream,
+  } = useReactMediaRecorder({ audio: true, askPermissionOnMount: true });
+
   let [emotion, setEmotion] = useState([
     {
-      name: "Model Name",
+      name: "CNN+LSTM (SUBESCO)",
       values: [0, 0, 0, 0, 0, 0, 0],
     },
   ]);
 
   console.log(emotion[0].name);
 
-  useEffect(() => {
-    const getEmotion = async (blob) => {
-      console.log("Making request from frontend", blob);
+  // useEffect(() => {
+  //   const getEmotion = async (blob) => {
+  //     console.log("Making request from frontend", blob);
 
-      const fd = new FormData();
-      fd.append("audio", blob);
+  //     const fd = new FormData();
+  //     fd.append("audio", blob);
 
-      fetch("http://localhost:8000/get-emo", {
-        method: "post",
-        body: fd,
-      })
-        .then((res) => res.json())
-        .then((data) => setEmotion(data.results))
-        .catch((err) => {
-          console.error("Failed to get emotion. ", err);
-        });
-    };
+  //     fetch("http://localhost:8000/get-emo", {
+  //       method: "post",
+  //       body: fd,
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => setEmotion(data.results))
+  //       .catch((err) => {
+  //         console.error("Failed to get emotion. ", err);
+  //       });
+  //   };
 
-    if (audioURL !== "") {
-      fetch(audioURL)
-        .then((response) => response.blob())
-        .then((data) => getEmotion(data))
-        .catch((err) =>
-          console.error("Failed to fetch blob from audio URL", err)
-        );
-    }
+  //   if (audioURL !== "") {
+  //     fetch(audioURL)
+  //       .then((response) => response.blob())
+  //       .then((data) => getEmotion(data))
+  //       .catch((err) =>
+  //         console.error("Failed to fetch blob from audio URL", err)
+  //       );
+  //   }
 
-    // Cleanup
-    return () => (audioURL = "");
-  }, [audioURL]);
+  //   // Cleanup
+  //   return () => (audioURL = "");
+  // }, [audioURL]);
 
   return (
     <div className="App">
       <h1>Real Time Speech Emotion Recognition</h1>
-      <audio className="mx-auto my-10" src={audioURL} controls />
-      <div className="text-3xl">
-        <button onClick={toggleRecording}>
-          {isRecording ? "🔴 Stop Recording" : "🎙️ Start Recording"}
-        </button>
+
+      <div>
+        <audio className="mx-auto my-10" src={mediaBlobUrl} controls />
+        <audio className="mx-auto my-10" src={previewAudioStream} controls />
+        <p className="mb-3">
+          {status === "acquiring_media" && "⏳ Acquiring media..."}
+          {status === "idle" && "🎙️ Ready for Input..."}
+          {status === "recording" && "🔴 Recording..."}
+          {status === "stopping" && "❕ Stopping..."}
+          {status === "stopped" && "❌ Stopped"}
+          {console.log("Recording status: ", status)}
+        </p>
+        <div className="text-3xl space-x-5">
+          <button
+            className={
+              status !== "recording" ? "btn-primary" : "btn-primary__disabled"
+            }
+            onClick={startRecording}
+            disabled={status === "recording"}
+          >
+            Start Speaking
+          </button>
+
+          <button
+            className={
+              status !== "recording" ? "btn-delete__disabled" : "btn-delete"
+            }
+            onClick={stopRecording}
+            disabled={status !== "recording"}
+          >
+            Stop Speaking
+          </button>
+        </div>
       </div>
+
       <div className="mt-5">
         <Bar
           data={{
