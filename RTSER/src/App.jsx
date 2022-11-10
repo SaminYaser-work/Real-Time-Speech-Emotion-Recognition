07e3db7e026284530c4bc31b634d43982027b086
Samin Yaser <saminyaserwork@gmail.com>
2022-11-10 15:02:59 +0600

    added hubert model

--- RTSER/src/App.jsx
+++ RTSER/src/App.jsx
@@ -4,216 +4,222 @@ import { useEffect, useState } from "react";
 import Timer from "./components/Timer";
 import { Bar } from "react-chartjs-2";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from "chart.js";
 // import loadingGif from "./assets/loading.gif";
 // import errorGif from "./assets/error.gif";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 const classes = [
-    "Angry 😠",
-    "Disgust 🤮",
-    "Fear 😨",
-    "Happy 😃",
-    "Neutral 😐",
-    "Sad 😭",
-    "Surprised 😲",
+  "Angry 😠",
+  "Disgust 🤮",
+  "Fear 😨",
+  "Happy 😃",
+  "Neutral 😐",
+  "Sad 😭",
+  "Surprised 😲",
 ];
 
 export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: "top",
-        },
-        title: {
-            display: true,
-            text: "Results",
-        },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
     },
-    scales: {
-        x: {
-            title: {
-                display: true,
-                text: "Emotions",
-                color: "blue",
-            },
-        },
-        y: {
-            title: {
-                display: true,
-                text: "Probability",
-                color: "blue",
-            },
-            ticks: {
-                callback: function (value, index, values) {
-                    return value + "%";
-                },
-            },
-        },
+    title: {
+      display: true,
+      text: "Results",
     },
+  },
+  scales: {
+    x: {
+      title: {
+        display: true,
+        text: "Emotions",
+        color: "black",
+      },
+    },
+    y: {
+      max: 100,
+      min: 0,
+      title: {
+        display: true,
+        text: "Probability",
+        color: "black",
+      },
+      ticks: {
+        // format: {
+        //   style: "percent",
+        // },
+        callback: function (value, index, values) {
+          return value + " %";
+        },
+        stepSize: 5,
+      },
+    },
+  },
 };
 
 const UPDATE_RATE = 2000;
 
 function App() {
-    const {
-        status,
-        error,
-        startRecording,
-        stopRecording,
-        mediaBlobUrl,
-        previewAudioStream,
-    } = useReactMediaRecorder({
-        audio: true,
-        askPermissionOnMount: true,
-        // blobPropertyBag: { type: "audio/webm" },
-    });
+  const {
+    status,
+    error,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+    previewAudioStream,
+  } = useReactMediaRecorder({
+    audio: true,
+    askPermissionOnMount: true,
+    // blobPropertyBag: { type: "audio/webm" },
+  });
 
-    let [emotion, setEmotion] = useState([
-        {
-            name: "CNN+LSTM (SUBESCO)",
-            values: [0, 0, 0, 0, 0, 0, 0],
-        },
-    ]);
+  let [emotion, setEmotion] = useState([
+    {
+      name: "...",
+      values: [0, 0, 0, 0, 0, 0, 0],
+    },
+    {
+      name: "...",
+      values: [0, 0, 0, 0, 0, 0, 0],
+    },
+  ]);
 
-    if (error) {
-        console.log(error);
+  if (error) {
+    console.log(error);
+  }
+
+  const getEmotion = async (blob) => {
+    console.log("Making request from frontend", blob);
+
+    const fd = new FormData();
+    fd.append("audio", blob);
+
+    try {
+      console.time("Request");
+      const res = await fetch("http://localhost:8000/get-emo", {
+        method: "post",
+        body: fd,
+      });
+      const data = await res.json();
+      console.timeEnd("Request");
+      setEmotion(data.results);
+      console.log(data.results);
+    } catch (err) {
+      console.error("Failed to get emotion. ", err);
     }
+  };
 
-    const getEmotion = async (blob) => {
-        console.log("Making request from frontend", blob);
-
-        const fd = new FormData();
-        fd.append("audio", blob);
-
-        try {
-            const res = await fetch("http://localhost:8000/get-emo", {
-                method: "post",
-                body: fd,
-            });
-            const data = await res.json();
-            console.log("Updating chart...");
-            setEmotion(data.results);
-            console.log(data.results);
-        } catch (err) {
-            console.error("Failed to get emotion. ", err);
-        }
+  const updateChart = () => {
+    let chunks = [];
+    const stream = new MediaRecorder(previewAudioStream);
+    stream.ondataavailable = (e) => {
+      chunks.push(e.data);
     };
 
-    const updateChart = () => {
-        let chunks = [];
-        const stream = new MediaRecorder(previewAudioStream);
-        stream.ondataavailable = (e) => {
-            chunks.push(e.data);
-        };
+    // Send to server
+    stream.onstop = (e) => getEmotion(new Blob(chunks));
 
-        // Send to server
-        stream.onstop = (e) => getEmotion(new Blob(chunks));
+    setTimeout(() => {
+      if (stream.state != "inactive") {
+        stream.stop();
+      }
+    }, UPDATE_RATE);
+    stream.start();
+  };
 
-        setTimeout(() => {
-            if (stream.state != "inactive") {
-                stream.stop();
-            }
-        }, UPDATE_RATE);
-        stream.start();
+  useEffect(() => {
+    if (status !== "recording") return;
+
+    let interval;
+    interval = setInterval(() => updateChart(), UPDATE_RATE);
+
+    return () => {
+      clearInterval(interval);
     };
+  }, [status]);
 
-    useEffect(() => {
-        if (status !== "recording") return;
+  return (
+    <div className="App">
+      <h1>Real Time Speech Emotion Recognition</h1>
 
-        let interval;
-        interval = setInterval(() => updateChart(), UPDATE_RATE);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [status]);
-
-    return (
-        <div className="App">
-            <h1>Real Time Speech Emotion Recognition</h1>
-
-            <div>
-                <audio
-                    className="mx-auto my-10"
-                    src={mediaBlobUrl}
-                    controls
-                />
-                <div className="text-2xl mb-3">
-                    <Timer status={status} />
-                </div>
-                <p className="mb-3">
-                    {status === "acquiring_media" &&
-                        "⏳ Getting permission..."}
-                    {status === "idle" && "🎙️ Ready for Input"}
-                    {status === "recording" && "🔴 Recording..."}
-                    {status === "stopping" && "❕ Stopping..."}
-                    {status === "stopped" && "❌ Stopped"}
-                    {/* {console.log("Recording status: ", status)} */}
-                </p>
-                <div className="text-3xl space-x-5">
-                    <button
-                        className={
-                            status !== "recording"
-                                ? "btn-primary"
-                                : "btn-primary__disabled"
-                        }
-                        onClick={startRecording}
-                        disabled={status === "recording"}
-                    >
-                        Start Speaking
-                    </button>
-
-                    <button
-                        className={
-                            status !== "recording"
-                                ? "btn-delete__disabled"
-                                : "btn-delete"
-                        }
-                        onClick={stopRecording}
-                        disabled={status !== "recording"}
-                    >
-                        Stop Speaking
-                    </button>
-                </div>
-            </div>
-
-            <div className="mt-5">
-                <Bar
-                    data={{
-                        labels: classes,
-                        backgroundColor: "rgba(255, 99, 132, 0.5)",
-                        datasets: [
-                            {
-                                label: emotion.name,
-                                data: emotion.values,
-                                // backgroundColor: "rgba(255, 99, 132, 0.5)",
-                                borderColor: "rgb(255, 99, 132)",
-                                backgroundColor:
-                                    "rgba(255, 99, 132, 0.5)",
-                            },
-                        ],
-                    }}
-                    options={options}
-                />
-            </div>
+      <div>
+        <audio className="mx-auto my-10" src={mediaBlobUrl} controls />
+        <div className="text-2xl mb-3">
+          <Timer status={status} />
         </div>
-    );
+        <p className="mb-3">
+          {status === "acquiring_media" && "⏳ Getting permission..."}
+          {status === "idle" && "🎙️ Ready for Input"}
+          {status === "recording" && "🔴 Recording..."}
+          {status === "stopping" && "❕ Stopping..."}
+          {status === "stopped" && "❌ Stopped"}
+          {/* {console.log("Recording status: ", status)} */}
+        </p>
+        <div className="text-3xl space-x-5">
+          <button
+            className={
+              status !== "recording" ? "btn-primary" : "btn-primary__disabled"
+            }
+            onClick={startRecording}
+            disabled={status === "recording"}
+          >
+            Start Speaking
+          </button>
+
+          <button
+            className={
+              status !== "recording" ? "btn-delete__disabled" : "btn-delete"
+            }
+            onClick={stopRecording}
+            disabled={status !== "recording"}
+          >
+            Stop Speaking
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <Bar
+          data={{
+            labels: classes,
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            datasets: [
+              {
+                label: emotion[0].name,
+                data: emotion[0].values,
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+              },
+              {
+                label: emotion[1].name,
+                data: emotion[1].values,
+                borderColor: "rgb(0, 71, 171)",
+                backgroundColor: "rgba(0, 150, 255, 0.5)",
+              },
+            ],
+          }}
+          options={options}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import librosa
 import trained_models.VGGish.predict as vggish
+import trained_models.HuBERT.predict as hubert
 
 
 classes = ['Angry 😠', 'Disgust 🤮', 'Fear 😨', 'Happy 😃',
@@ -10,7 +11,10 @@ def get_emo(path):
     y, sr = librosa.load(path, sr=16000)
 
     vggish_res = vggish.get_emotion(y, sr)
+    hubert_res = hubert.get_emotion(y, sr)
+    print(vggish_res)
+    print(hubert_res)
 
     return {
-        "results": vggish_res
+        "results": [vggish_res, hubert_res]
     }
